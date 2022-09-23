bac93d71742d726de53ceec91a16d5caa6c6f0ef
Chris <kjman26@gmail.com>
2022-09-22 18:22:48 -0700

    updates to html

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,17 +1,37 @@
 var APIKey = "9073278d26315ddad5d7b4c589fbb8c2";
 var city = $('#cities').val();
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=" + APIKey;
+// var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+// var weatherThis = [
+//   {
+//     main: '',
+//     description: '',
+//     icon: [],
+//   }
+// ];
+
+
+
 
 //fetch request
 function getWeather(){
-    fetch(queryUrl)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=" + APIKey)
       .then(response => {
         return response.json();
           })
       .then(data => {
-        localStorage.setItem('practice', JSON.stringify(data.main), (data.weather))
-        console.log(JSON.stringify(data.main),(data.weather))
-        $('#cityName').text(JSON.stringify(data.main))
+        localStorage.setItem('practice', JSON.stringify(data))
+
+        console.log(JSON.stringify(data))
+
+        document.querySelector('#cityName').innerText = data.name
+        document.querySelector("#weatherInfo") = data.weather
+        // ,(JSON.stringify(data.name))
+      
+      
+        console.log(weatherThis)
+
       });
       // .then(data.main => {
         

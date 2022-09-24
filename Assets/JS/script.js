f7f5525a8a709eebd342b16a671e915d9bab3b8b
Chris <kjman26@gmail.com>
2022-09-24 10:07:36 -0700

    updated fetch, now assigning variables and text

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,46 +1,49 @@
 var APIKey = "9073278d26315ddad5d7b4c589fbb8c2";
-var city = $('#cities').val();
-// var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-
-// var weatherThis = [
-//   {
-//     main: '',
-//     description: '',
-//     icon: [],
-//   }
-// ];
 
 
 
 
+
+// get city
+// function getCity(){
+  
+//   fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey)
+//   console.log(city)
+// }
 //fetch request
 function getWeather(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=" + APIKey)
+    let city = $('#cities').val();
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey)
       .then(response => {
         return response.json();
           })
       .then(data => {
         localStorage.setItem('practice', JSON.stringify(data))
 
-        console.log(JSON.stringify(data))
-
+        console.log(JSON.stringify(data.main))
+// changing text content
         document.querySelector('#cityName').innerText = data.name
-        document.querySelector("#weatherInfo") = data.weather
-        // ,(JSON.stringify(data.name))
-      
-      
-        console.log(weatherThis)
+        document.querySelector("#weatherInfo").innerText = JSON.stringify(data.weather[0].description)
+       
+         
+        // let icon = "http://openweathermap.org/img/wn/" + icon + ".png"
+        // document.querySelector("#icon").innerText = 
+        console.log(data.weather[0].icon)
 
-      });
+
+        });
       // .then(data.main => {
-        
+      // $('#icon').attr((
+      //     "src" = "http://openweathermap.org/img/wn/" + icon + ".png"
+
+      //   ))
+      
       // }
  };
 
  //on click function
 $('#search').on("click", getWeather);
-console.log(city)
+
 
 
 

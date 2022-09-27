06f7764cffea4bf846ad2f780c887c7a40a7c5ff
Chris <kjman26@gmail.com>
2022-09-26 17:51:56 -0700

    added daily weather function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -17,50 +17,50 @@ function getLatLong(){
 
 
 // fetching 5 day forecast
-function getForecast(){
-let lat = localStorage.getItem("lat","");
-let lon = localStorage.getItem("lon","");
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + {lat} + "&lon=" + {lon} + "&exclude=hourly,daily&units=imperial&appid=" + APIKey)
-  .then(response => {
-    return response.json();
-  })
-    .then(data => {
-      console.log(JSON.stringify(data))
-    })
-};
+// function getForecast(){
+// let lat = localStorage.getItem("lat","");
+// let lon = localStorage.getItem("lon","");
+// fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + {lat} + "&lon=" + {lon} + "&exclude=hourly,daily&units=imperial&appid=" + APIKey)
+//   .then(response => {
+//     return response.json();
+//   })
+//     .then(data => {
+//       console.log(JSON.stringify(data))
+//     })
+// };
 
 
 
 //fetch request for daily weather
-// function getWeather() {
-//   let city = $('#cities').val();
-//   var dailyInfo = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey)
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(data => {
-//       localStorage.setItem('practice', JSON.stringify(data))
-//       console.log(JSON.stringify(data.main))
-//       // changing text content
-//       document.querySelector('#cityName').innerText = data.name;
-//       document.querySelector("#weatherInfo").innerText = "Conditions: " + data.weather[0].description;
-//       document.querySelector("#humidity").innerText = "Humidity: " + JSON.stringify(data.main.humidity);
-//       document.querySelector("#windSpeed").innerText = "Wind Speed: " + JSON.stringify(data.wind.speed); 
-//       document.querySelector("#temp").innerText = "Current temp: " + JSON.stringify(data.main.temp) + "°";
-//       let icon = data.weather[0].icon
-//       $('#icon').attr(
-//         "src", "http://openweathermap.org/img/wn/" + icon + ".png",
-//         "alt", "weather icon"
-//       );
-//       });
-//   };
+function getWeather() {
+  let city = $('#cities').val();
+  var dailyInfo = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      localStorage.setItem('practice', JSON.stringify(data))
+      console.log(JSON.stringify(data.main))
+      // changing text content
+      document.querySelector('#cityName').innerText = data.name;
+      document.querySelector("#weatherInfo").innerText = "Conditions: " + data.weather[0].description;
+      document.querySelector("#humidity").innerText = "Humidity: " + JSON.stringify(data.main.humidity);
+      document.querySelector("#windSpeed").innerText = "Wind Speed: " + JSON.stringify(data.wind.speed); 
+      document.querySelector("#temp").innerText = "Current temp: " + JSON.stringify(data.main.temp) + "°";
+      let icon = data.weather[0].icon
+      $('#icon').attr(
+        "src", "http://openweathermap.org/img/wn/" + icon + ".png",
+        "alt", "weather icon"
+      );
+      });
+  };
 let date = moment().format("MMM, DD, YYYY");
       $("#date").text(date)
 
 
 
 //on click function
-$('#search').on("click", getLatLong, getForecast);
+$('#search').on("click", getWeather);
 
 
 
